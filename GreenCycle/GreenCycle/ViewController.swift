81595bf186b719dcf91240c0dad0a4b1feed65ad
Eunice991217 <101406317+Eunice991217@users.noreply.github.com>
2023-11-29 10:47:23 +0900

    feat : 기본 세팅 (색상 지정, 앱 아이콘 설정)

---
+++ GreenCycle/GreenCycle/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GreenCycle
+//
+//  Created by 김민경 on 11/29/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
